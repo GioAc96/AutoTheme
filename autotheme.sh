cc56b5096d89ee725f3b04c987cb3e45d29baaa0
GioAc96 <gioac96@gmail.com>
2016-10-14 13:39:06 +0200

    Update autotheme.sh

--- autotheme.sh
+++ autotheme.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
+DARK='Arc-Dark'
+BRIGHT='Arc'
 
 godark() {
 	notify-send "Going Dark!"
-	gsettings set org.gnome.desktop.interface gtk-theme 'Arc-Dark'
-	gsettings set org.gnome.desktop.wm.preferences theme 'Arc-Dark'
+	gsettings set org.gnome.desktop.interface gtk-theme $DARK
+	gsettings set org.gnome.desktop.wm.preferences theme $DARK
 	gsettings set org.gnome.gedit.preferences.editor scheme 'cobalt'
 }
 
 gobright() {
 	notify-send "Going Bright!"
-	gsettings set org.gnome.desktop.interface gtk-theme 'Arc'
-	gsettings set org.gnome.desktop.wm.preferences theme 'Arc'
+	gsettings set org.gnome.desktop.interface gtk-theme $BRIGHT
+	gsettings set org.gnome.desktop.wm.preferences theme $BRIGHT
 	gsettings set org.gnome.gedit.preferences.editor scheme 'kate'	
 }
 
 hour=$(date "+%-H")
 curtheme=$(gsettings get org.gnome.desktop.interface gtk-theme)
 if [ "$hour" -gt "21" ] || [ $hour -lt "7" ]; then
-	if [ "$curtheme" != "'Arc-Dark'" ]; then
+	if [ "$curtheme" != "$DARK" ]; then
 		godark
 		notify-send "Going dark!"
 	fi
 else
-	if [ "$curtheme" != "'Arc'" ]; then
+	if [ "$curtheme" != "$BRIGHT" ]; then
 		gobright 
 		notify-send "Going bright!"
 	fi
